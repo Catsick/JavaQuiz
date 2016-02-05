32770869fb965210ca4a1aff42865b0a0196cf7c
Bonnie Smith <bmr28@wildcats.unh.edu>
2016-02-04 20:14:37 -0500

    FIxed issues.

--- app/src/main/java/com/example/bonnie/javaquiz/JavaQuiz.java
+++ app/src/main/java/com/example/bonnie/javaquiz/JavaQuiz.java
@@ -2,12 +2,32 @@ package com.example.bonnie.javaquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JavaQuiz extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_buttin);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.true_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
